4bcb6db50b43806b4d3502001c08a277926b7fa6
bnantares <ant8151@yandex.ru>
2024-12-20 17:36:41 +0300

    Form refactoring

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,6 +3,8 @@ import {
   Checkbox,
   Textarea,
   TextInput,
+  Radio,
+  DatePickerInput
 } from "react-hook-form-mantine";
 import { Button, Group, Paper, Container, Stack } from "@mantine/core";
 import { DevTool } from "@hookform/devtools";
@@ -11,25 +13,32 @@ type Inputs = {
     checkbox: boolean
     textarea: string
     textInput: string
+    radio: string
+    datepicker: Date | null
 }
 
 export default function App() {
   const { 
     control,
-    handleSubmit
+    handleSubmit,
+    reset,
   } = useForm<Inputs>({
     defaultValues: {
       checkbox: false,
       textarea: "",
-      textInput: ""
+      textInput: "",
+      radio: "",
+      datepicker: null
     }
   });
 
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
-
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+    reset()
+  }
   return (
-    <div className="App">
-      <Container size={1000}>
+    <div className="App" style={{marginRight: "300px"}}>
+      <Container fluid>
         <Paper withBorder shadow="md" p={30} mt={30} radius="md">
           <Form
             control={control}
@@ -49,7 +58,7 @@ export default function App() {
                 placeholder="Your comment"
                 label="Your comment"
                 required
-                rules={{ required: "Обязательное поле" }}
+                rules={{ required: "Field is required" }}
                 control={control}
               />
               <TextInput
@@ -57,10 +66,28 @@ export default function App() {
                 placeholder="Your name"
                 label="Full name"
                 required
-                rules={{ required: "Обязательное поле" }}
+                rules={{ required: "Field is required" }}
                 control={control}
               />
-
+              <Radio.Group
+                name="radio"
+                control={control}
+                label="Select one"
+                description="This is selectable"
+              >
+                <Group mt="xs">
+                  <Radio.Item value="1" label="1" />
+                  <Radio.Item value="2" label="2" />
+                  <Radio.Item value="3" label="3" />
+                  <Radio.Item value="4" label="4" />
+                </Group>
+              </Radio.Group>
+              <DatePickerInput
+                name="datepicker" 
+                label="Pick date"
+                placeholder="Pick date"
+                control={control}
+              />
               <Group mt="md">
                 <Button type="submit">Submit</Button>
               </Group>
